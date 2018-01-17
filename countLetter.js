134cb9303f710a864a41f56d875ee05fc6ee9d90
ravraw <ravinrawat.wd@gmail.com>
2018-01-17 21:47:02 +0000

    recommiting the files

--- countLetter.js
+++ countLetter.js
@@ -1,5 +1,4 @@
 
-
 var myArgv = process.argv.slice(2);
 
 function countLetters(sampleString){
@@ -8,33 +7,28 @@ function countLetters(sampleString){
   var inputString = sampleString.join('');
   var splitArray = inputString.split("");
   var filteredArray = splitArray.filter(function(elem, index, self){
-      return index == self.indexOf(elem)
-      });
+    return index === self.indexOf(elem);
+  });
 
   var occuranceCount = [];
   var count = 0 ;
 
   for (var i = 0 ; i < filteredArray.length ; i++){
     for(var j = 0 ; j < splitArray.length ; j++){
-       if(filteredArray[i] === splitArray[j]){
-         count += 1 ;
-        }
+      if(filteredArray[i] === splitArray[j]){
+        count += 1 ;
+      }
     }
     occuranceCount.push(count);
     count = 0;
-  };
+  }
 
   for(let i = 0 ;i < occuranceCount.length ;i++){
     objLetterCount[filteredArray[i]] = occuranceCount[i];
   }
   return objLetterCount ;
-};
+}
 
 
 var results = countLetters(myArgv);
 console.log(results);
-
-
-
-
-
